6ddbc5e9575baf368bd20a173fd259bd8d80dea9
Scott Medefind <smedefind@optoro.com>
2015-11-13 14:27:14 -0500

    Increasing unicorn timeout to 300 sec

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,3 +2,4 @@ default['geminabox']['rubygems_proxy'] = true
 default['gem_server']['allow_remote_failure'] = true
 default['geminabox']['disks'] = ['/dev/xvdf']
 default['geminabox']['nginx']['bind'] = '0.0.0.0'
+default['geminabox']['unicorn']['timeout'] = 300
