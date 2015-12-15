11dcda2906caa573b1e344463d1d4815dacea33c
Jeff Gillis <jgillis@optoro.com>
2015-12-15 12:15:31 -0500

    Update nginx cookbook to support json_format logging

--- Berksfile
+++ Berksfile
@@ -5,6 +5,5 @@ cookbook 'optoro_ruby', '~> 1.0.3'
 cookbook 'zpool', '~> 0.1.0'
 cookbook 'optoro_zfs', '~> 0.0.1'
 cookbook 'aws', '~> 2.6.4'
-cookbook 'nginx', '= 2.7.4'
 
 metadata
--- metadata.rb
+++ metadata.rb
@@ -4,11 +4,11 @@ maintainer 'devops@optoro.com'
 license 'MIT'
 description 'Installs and configures gem in a box'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version '0.0.4'
+version '0.0.5'
 
 depends 'geminabox'
 depends 'aws'
 depends 'optoro_ruby'
 depends 'zpool'
 depends 'optoro_zfs'
-depends 'nginx', '= 2.7.4'
+depends 'nginx', '~> 2.7'
