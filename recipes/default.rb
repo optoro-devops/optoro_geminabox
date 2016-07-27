d7f09b8c6725b30660a68b5615e0bcb04b8c3d5b
Scott Medefind <smedefind@optoro.com>
2016-07-27 12:09:12 -0400

    Adding EC2 conditionals

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,23 +1,26 @@
 include_recipe 'geminabox'
-include_recipe 'optoro_zfs'
-include_recipe 'aws'
 
-aws_ebs_volume 'geminabox_cache' do
-  size 50
-  device '/dev/sdf'
-  action [:create, :attach]
-end
+if node['ec2']
+  include_recipe 'optoro_zfs'
+  include_recipe 'aws'
 
-zpool 'geminabox_cache' do
-  disks node['geminabox']['disks']
-  mountpoint 'none'
-  force true
-end
+  aws_ebs_volume 'geminabox_cache' do
+    size 50
+    device '/dev/sdf'
+    action [:create, :attach]
+  end
 
-zfs 'geminabox_cache/data' do
-  mountpoint "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}"
-  atime 'off'
-  compression 'lz4'
+  zpool 'geminabox_cache' do
+    disks node['geminabox']['disks']
+    mountpoint 'none'
+    force true
+  end
+
+  zfs 'geminabox_cache/data' do
+    mountpoint "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}"
+    atime 'off'
+    compression 'lz4'
+  end
 end
 
 directory "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}" do
