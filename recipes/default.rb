fafbf5f4a35a6eaada3896fdf0b539e0c39483c0
Alex Freidah <afreidah@optoro.com>
2017-04-10 11:24:32 -0400

    disable any default or 000-default sites and restart nginx if so

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,27 +1,38 @@
 include_recipe 'geminabox'
-include_recipe 'optoro_zfs'
-include_recipe 'aws'
 
-aws_ebs_volume 'geminabox_cache' do
-  size 50
-  device '/dev/sdf'
-  action [:create, :attach]
+# disable any default sites that get resurrected by upgrades
+%w(000-default default).each do |site|
+  nginx_site site do
+    enable false
+    notifies :restart, 'service[nginx]'
+  end
 end
 
-zpool 'geminabox_cache' do
-  disks node['geminabox']['disks']
-  mountpoint 'none'
-  force true
-end
+if node['ec2']
+  include_recipe 'optoro_zfs'
+  include_recipe 'aws'
 
-zfs 'geminabox_cache/data' do
-  mountpoint "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}"
-  atime 'off'
-  compression 'lz4'
-end
+  aws_ebs_volume 'geminabox_cache' do
+    size 50
+    device '/dev/sdf'
+    action [:create, :attach]
+  end
 
-directory "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}" do
-  owner node['geminabox']['www_user']
-  group node['geminabox']['www_user']
-  recursive true
+  zpool 'geminabox_cache' do
+    disks node['geminabox']['disks']
+    mountpoint 'none'
+    force true
+  end
+
+  zfs 'geminabox_cache/data' do
+    mountpoint "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}"
+    atime 'off'
+    compression 'lz4'
+  end
+
+  directory "#{node['geminabox']['base_directory']}/#{node['geminabox']['data_directory']}" do
+    owner node['geminabox']['www_user']
+    group node['geminabox']['www_user']
+    recursive true
+  end
 end
--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,13 +1,5 @@
 require 'spec_helper'
 
-describe zfs('geminabox_cache/data') do
-  it { should exist }
-end
-
-describe zfs('geminabox_cache/data') do
-  it { should have_property 'mountpoint' => '/var/www/geminabox/data', 'compression' => 'lz4', 'atime' => 'off' }
-end
-
 describe port(80) do
   it { should be_listening }
 end
