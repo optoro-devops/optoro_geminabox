780924fb23529c9f7495320f8ec3a44ffc3ef8df
Daniel Vandenberg <dvandenberg@optoro.com>
2017-07-12 23:17:12 -0400

    Fix backup script variables

--- attributes/default.rb
+++ attributes/default.rb
@@ -4,5 +4,5 @@ default['geminabox']['disks'] = ['/dev/xvdf']
 default['geminabox']['nginx']['bind'] = '0.0.0.0'
 default['geminabox']['unicorn']['timeout'] = 300
 
-default['optoro_geminabox']['backup']['data_dir'] = '/var/www/geminabox/data'
+default['optoro_geminabox']['backup']['dir'] = '/var/www/geminabox/data'
 default['optoro_geminabox']['backup']['s3_bucket'] = 'optoro-db-backups'
